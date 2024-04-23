e4081867885d9dec0c9912c17da585958e3966a1
Sanskari Wolf <tanushpurwar@gmail.com>
2024-04-23 12:56:42 +0530

    Adding info

--- src/Data/projectsData.js
+++ src/Data/projectsData.js
@@ -8,9 +8,9 @@ export const projectsData = [
         photo: 'https://via.placeholder.com/300',
         github: 'https://github.com/singhchanmeet/erp',
         live: 'https://www.project1.com',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        techStack: 'React, Tailwind CSS, Node.js',
-        contributors: ['Contributor 1', 'Contributor 2'],
+        description: 'Enterprise Resource Planning System for college.',
+        techStack: 'React, Tailwind CSS, Node.js, Django, MariaDB',
+        contributors: ['Aman', 'Chanmeet', 'Ishaan', 'Subhasish', 'Haaris', 'Shivank'],
         screenshots: ['https://via.placeholder.com/300', 'https://via.placeholder.com/300']
       },
       {
@@ -19,9 +19,9 @@ export const projectsData = [
         photo: 'https://via.placeholder.com/300',
         github: 'https://github.com/exploring-solver/ByteLocker',
         live: 'https://www.project2.com',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        techStack: 'React, Tailwind CSS, Node.js',
-        contributors: ['Contributor 1', 'Contributor 2'],
+        description: 'A college security and attendance system utilizing facial recognition to track and mark attendance with live feed tracking.',
+        techStack: 'flask, python, cpp',
+        contributors: ['Mayank', 'Pratham', 'Sagar Jain', 'Tanush', 'Harshill','Aman','Abir'],
         screenshots: ['https://via.placeholder.com/300', 'https://via.placeholder.com/300']
       },
       {
@@ -30,11 +30,22 @@ export const projectsData = [
         photo: 'https://via.placeholder.com/300',
         github: 'https://github.com/IshaanMinocha/byte-3d-website',
         live: 'https://www.project2.com',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        techStack: 'React, Tailwind CSS, Node.js',
-        contributors: ['Contributor 1', 'Contributor 2'],
+        description: 'Website for our society',
+        techStack: 'React, 3js',
+        contributors: ['Ishaan', 'Aman', 'Harshill', 'Khushi', 'Adisht', 'Garvit'],
         screenshots: ['https://via.placeholder.com/300', 'https://via.placeholder.com/300']
       },
+      {
+        id: 3,
+        name: 'SereneHub',
+        photo: 'https://via.placeholder.com/300',
+        github: 'https://github.com/IshaanMinocha/byte-3d-website',
+        live: 'https://www.project2.com',
+        description: 'Discover personalized well-being journeys through our innovative platform, combining technology and empathy. Experience real-time emotional analysis, mood-based song suggestions, multilingual chatbot support with realtime speech and text mood analysis, and SereneHuman for an intelligent virtual companion for empathy and mental wellness.',
+        techStack: 'python, tensorflow(API), unreal engine, AI meta human library',
+        contributors: ['Subhasish Panda', 'Abir Thakur', 'Shivam Kumar Jha'],
+        screenshots: ['https://via.placeholder.com/300', 'https://via.placeholder.com/300']
+      }
     ]
   },
   {
@@ -46,9 +57,9 @@ export const projectsData = [
         photo: 'https://via.placeholder.com/300',
         github: 'https://github.com/singhchanmeet/ClickPick',
         live: 'https://www.project3.com',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        techStack: 'React, Tailwind CSS, Node.js',
-        contributors: ['Contributor 1', 'Contributor 2'],
+        description: 'A customised booking/ordering mobile app being made for MAIT's stationery and canteens',
+        techStack: 'Redjango react native',
+        contributors: ['Aman Sharma', 'Ravi Banchiwal', 'Abir Thakur', 'Chanmeet Singh'],
         screenshots: ['https://via.placeholder.com/300', 'https://via.placeholder.com/300']
       },
     ]
@@ -64,7 +75,7 @@ export const projectsData = [
         live: 'https://www.project3.com',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
         techStack: 'React, Tailwind CSS, Node.js',
-        contributors: ['Contributor 1', 'Contributor 2'],
+        contributors: ['Chayan Mann', 'Rachit', 'Nalin', 'Tanush', 'Shivam Gupta', 'Chayan Khetan', 'Pratham Gupta', 'Ashish', 'Piyush'],
         screenshots: ['https://via.placeholder.com/300', 'https://via.placeholder.com/300']
       },
       {
@@ -73,9 +84,9 @@ export const projectsData = [
         photo: 'https://via.placeholder.com/300',
         github: 'https://github.com/username/project4',
         live: 'https://www.project3.com',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        description: 'This research proposes a new method that uses existing Wi-Fi signals to estimate body pose. By analysing the strength and direction of Wi-Fi penetrating through human regions (i.e. Dampening of signals), the system can create a heat map showing distorted body position, on which DensePose model is applied to predict the exact structure and position of a human figure ',
         techStack: 'React, Tailwind CSS, Node.js',
-        contributors: ['Contributor 1', 'Contributor 2'],
+        contributors: ['Chayan Mann' , 'Shivam Kumar Jha', 'Ishaan' ,'Agrawal', 'Ashish Negi' , 'Nalin' , 'Anushka Singh', 'Ujjwal Gupta', 'Arnav'],
         screenshots: ['https://via.placeholder.com/300', 'https://via.placeholder.com/300']
       },
       {
@@ -84,11 +95,22 @@ export const projectsData = [
         photo: 'https://via.placeholder.com/300',
         github: 'https://github.com/username/project4',
         live: 'https://www.project3.com',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        description: 'Ionic thruster is a type of propulsion system that generates thrust by accelerating ions to high speeds and expelling them out of the back of the engine. The basic principle behind an ionic thruster is to use an electric field to ionize and accelerate a propellant to produce a small but continuous thrust.',
         techStack: 'React, Tailwind CSS, Node.js',
-        contributors: ['Contributor 1', 'Contributor 2'],
+        contributors: ['Rachit', 'Tanush','Ashish', 'Mayank', 'Tejaswi', 'Arnav Chopra'],
         screenshots: ['https://via.placeholder.com/300', 'https://via.placeholder.com/300']
       },
+      {
+        id: 7,
+        name: 'MircoMouse',
+        photo: 'https://via.placeholder.com/300',
+        github: 'https://github.com/username/project4',
+        live: 'https://www.project3.com',
+        description: 'Micromouse is a robotics competition that challenges participants to design and build autonomous robots capable of navigating through a 16x16 maze composed of grid cells. The objective is to find the shortest possible path from a predetermined starting point to the center of the maze. These robots must rely solely on their sensors and algorithms to map the maze, detect obstacles, and determine the most e cient route. Competitions measure the time taken by the robots to solve the maze as well as the algorithmic e ciency of the robot.',
+        techStack: 'React, Tailwind CSS, Node.js',
+        contributors: ['Chayan Mann', 'Rachit', 'Nalin', 'Tanush', 'Shivam Gupta', 'Chayan Khetan', 'Pratham Gupta', 'Ashish', 'Piyush'],
+        screenshots: ['https://via.placeholder.com/300', 'https://via.placeholder.com/300']
+      }
     ]
   },
   {
@@ -133,9 +155,20 @@ export const projectsData = [
         photo: '',
         github: '',
         live: '',
-        description: ''.
-        techStack: '',
-        contributors: [''],
+        description: '3D model of MAIT in metaverse',
+        techStack: 'Blender',
+        contributors: ['Aman', 'Ishaan', 'Abir'],
+        screenshots: ['']
+      },
+      {
+        id: 11,
+        name: 'MR Glasses',
+        photo: '',
+        github: '',
+        live: '',
+        description: 'A Headset/Glasses for Extended Reality(XR) and Mixed Reality(MR) spatial computing for running hardware and scientific simulations, inbuilt and downloadable apps for daily learning endeavours.',
+        techStack: 'Blender',
+        contributors: ['Subhasish Panda', 'Aman Sharma', 'Shivam Kumar Jha', 'Abir Thakur', 'Chayan Khetan'],
         screenshots: ['']
       }
     ]
